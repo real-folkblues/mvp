157c06d45f224532df39a8f23aaeff29333c5616
real-folkblues <marenicolor@gmail.com>
2024-01-28 19:21:26 -0500

    front end fires up successfully. experiencing compile errors with blogs but could see intended messages in the console. decided on api for app, listennotes.

---
+++ front-end/app/api/blogs/route.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NextResponse } from 'next/server';
+
+export const GET = async (req: Request, res: Response)=> {
+console.log("GET REQUEST");
+};
+
+
+
+export const POST = async (req: Request, res: Response) => {
+  console.log("POSTREQUEST");
+};
+
+
---
+++ front-end/app/api/lib/data.ts
@@ -0,0 +1,7 @@
+// type Post {
+//     id: string;
+//     title: string;
+//     desc: string;
+//     date: Date;
+// }
+// let posts: Post[] = [];
