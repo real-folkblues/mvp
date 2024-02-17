719cc9de4047954db52c158aa4094c1dc14d218c
real-folkblues <marenicolor@gmail.com>
2024-02-17 11:48:29 -0500

    testing ekispert api

--- front-end/app/lib/data.jsx
+++ front-end/app/lib/data.jsx
@@ -1,10 +1,70 @@
-// type Post {
-//     id: string;
-//     title: string;
-//     desc: string;
-//     date: Date;
-// }
-// let posts: Post[] = [];
-// {
-//     "name"; "John Doe"
-// }
+<ResultSet apiVersion="1.14.0.0" engineVersion="201404_01a">
+  <Course searchType="plain" dataType="plain">
+    <Route index="1" timeOnBoard="12" timeWalk="0" timeOther="12" transferCount="0" distance="25" exhaustCO2="127" exhaustCO2atPassengerCar="420">
+      <Point index="1">
+        <Prefecture code="13">
+          <Name>東京都</Name>
+        </Prefecture>
+        <GeoPoint gcs="tokyo" lati_d="35.655499" longi_d="139.705610" lati="35.39.19.79" longi="139.42.20.19"/>
+        <Station code="35656">
+          <Name>渋谷駅前(東側)／都営バス</Name>
+          <Yomi>しぶやえきまえ</Yomi>
+          <Type detail="local">bus</Type>
+        </Station>
+      </Point>
+      <Line index="1" fareIndex="1" teiki1Index="1" teiki3Index="1" timeOnBoard="12" distance="25" stopStationCount="0" exhaustCO2="127" exhaustCO2atPassengerCar="420" direction="Down">
+        <Name>都営バス・ＲＨ０１</Name>
+        <Type detail="local">bus</Type>
+        <TypicalName>都営バス・ＲＨ０１</TypicalName>
+        <Color>185043146</Color>
+        <DepartureState>
+          <Type>normal</Type>
+          <Datetime operation="today">2014-03-24</Datetime>
+        </DepartureState>
+        <ArrivalState>
+          <Type>normal</Type>
+          <Datetime operation="today">2014-03-24</Datetime>
+        </ArrivalState>
+        <TimeReliability>none</TimeReliability>
+        <Destination/>
+      </Line>
+      <Point index="2">
+        <Prefecture code="13">
+          <Name>東京都</Name>
+        </Prefecture>
+        <GeoPoint gcs="tokyo" lati_d="35.657165" longi_d="139.732610" lati="35.39.25.79" longi="139.43.57.39"/>
+        <Station code="36824">
+          <Name>六本木ヒルズ／都営バス</Name>
+          <Yomi>ろっぽんぎひるず</Yomi>
+          <Type detail="local">bus</Type>
+        </Station>
+      </Point>
+    </Route>
+    <Price kind="FareSummary">
+      <Oneway>200</Oneway>
+      <Round>400</Round>
+    </Price>
+    <Price kind="Fare" index="1" fromLineIndex="1" toLineIndex="1" fareRevisionStatus="none" selected="true">
+      <Oneway>200</Oneway>
+      <Round>400</Round>
+      <Type>Fare</Type>
+    </Price>
+    <Price kind="Teiki1Summary">
+      <Oneway>9000</Oneway>
+    </Price>
+    <Price kind="Teiki1" index="1" fromLineIndex="1" toLineIndex="1" fareRevisionStatus="none" selected="true">
+      <Oneway>9000</Oneway>
+      <Type>Teiki1</Type>
+    </Price>
+    <Price kind="Teiki3Summary">
+      <Oneway>25650</Oneway>
+    </Price>
+    <Price>
+    :
+    </Price>
+    <SerializeData>SHlUczAxLjAwLjA0rAQAAAAAJFEzAQEAAQAAAAAACAABAgACAAADAAAAAAAAAAEASIsAANiPAAAAAAAACgAWAAA2BJoAAAAAJFEzAf__________AgABAAEAAQABAAEAAAAAAAAA--T3221233232319:F3321122120:A23122141:--81c5c856d23710bdc1e8cbdac4989af6518cabcc</SerializeData>
+  </Course>
+  <Course>
+    :
+  </Course>
+</ResultSet>
---
+++ front-end/app/lib/sample.jsx
@@ -0,0 +1,143 @@
+{
+  "ResultSet": {
+    "apiVersion": "1.14.0.0",
+    "engineVersion": "201404_01a",
+    "Course": [
+      {
+        "searchType": "plain",
+        "dataType": "plain",
+        "Route": {
+          "timeOnBoard": "12",
+          "timeWalk": "0",
+          "timeOther": "12",
+          "transferCount": "0",
+          "distance": "25",
+          "exhaustCO2": "127",
+          "exhaustCO2atPassengerCar": "420",
+          "Point": [
+            {
+              "Prefecture": {
+                "code": "13",
+                "Name": "東京都"
+              },
+              "GeoPoint": {
+                "gcs": "tokyo",
+                "lati_d": "35.655499",
+                "longi_d": "139.705610",
+                "lati": "35.39.19.79",
+                "longi": "139.42.20.19"
+              },
+              "Station": {
+                "code": "35656",
+                "Name": "渋谷駅前(東側)／都営バス",
+                "Yomi": "しぶやえきまえ",
+                "Type": {
+                  "text": "bus",
+                  "detail": "local"
+                }
+              }
+            },
+            {
+              "Prefecture": {
+                "code": "13",
+                "Name": "東京都"
+              },
+              "GeoPoint": {
+                "gcs": "tokyo",
+                "lati_d": "35.657165",
+                "longi_d": "139.732610",
+                "lati": "35.39.25.79",
+                "longi": "139.43.57.39"
+              },
+              "Station": {
+                "code": "36824",
+                "Name": "六本木ヒルズ／都営バス",
+                "Yomi": "ろっぽんぎひるず",
+                "Type": {
+                  "text": "bus",
+                  "detail": "local"
+                }
+              }
+            }
+          ],
+          "Line": {
+            "fareIndex": "1",
+            "teiki1Index": "1",
+            "teiki3Index": "1",
+            "timeOnBoard": "12",
+            "distance": "25",
+            "stopStationCount": "0",
+            "exhaustCO2": "127",
+            "exhaustCO2atPassengerCar": "420",
+            "direction": "Down",
+            "Name": "都営バス・ＲＨ０１",
+            "Type": {
+              "text": "bus",
+              "detail": "local"
+            },
+            "TypicalName": "都営バス・ＲＨ０１",
+            "Color": "185043146",
+            "DepartureState": {
+              "Type": "normal",
+              "Datetime": {
+                "text": "2014-03-24",
+                "operation": "today"
+              }
+            },
+            "ArrivalState": {
+              "Type": "normal",
+              "Datetime": {
+                "text": "2014-03-24",
+                "operation": "today"
+              }
+            },
+            "TimeReliability": "none"
+          }
+        },
+        "Price": [
+          {
+            "kind": "FareSummary",
+            "Oneway": "200",
+            "Round": "400"
+          },
+          {
+            "kind": "Fare",
+            "index": "1",
+            "fromLineIndex": "1",
+            "toLineIndex": "1",
+            "fareRevisionStatus": "none",
+            "selected": "true",
+            "Oneway": "200",
+            "Round": "400",
+            "Type": "Fare"
+          },
+          {
+            "kind": "Teiki1Summary",
+            "Oneway": "9000"
+          },
+          {
+            "kind": "Teiki1",
+            "index": "1",
+            "fromLineIndex": "1",
+            "toLineIndex": "1",
+            "fareRevisionStatus": "none",
+            "selected": "true",
+            "Oneway": "9000",
+            "Type": "Teiki1"
+          },
+          {
+            "kind": "Teiki3Summary",
+            "Oneway": "25650"
+          },
+          {
+            :
+          }
+        ],
+        "SerializeData": "SHlUczAxLjAwLjA0rAQAAAAAJFEzAQEAAQAAAAAACAABAgACAAADAAAAAAAAAAEASIsAANiPAAAAAAAACgAWAAA2BJoAAAAAJFEzAf__________AgABAAEAAQABAAEAAAAAAAAA--T3221233232319:F3321122120:A23122141:--81c5c856d23710bdc1e8cbdac4989af6518cabcc"
+      },
+      {
+        :
+      }
+    ]
+  }
+}
