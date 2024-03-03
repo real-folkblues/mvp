3ebd7803354cb9c850f6f122051371f1d2ba3187
real-folkblues <marenicolor@gmail.com>
2024-03-03 13:58:00 -0500

    getting rid of DTO, added array fields

--- src/main/java/org/launchcode/mvp/controllers/KanjiController.java
+++ src/main/java/org/launchcode/mvp/controllers/KanjiController.java
@@ -1,57 +1,23 @@
 package org.launchcode.mvp.controllers;
 
-import jakarta.servlet.http.HttpSession;
 import org.launchcode.mvp.data.KanjiRepository;
 import org.launchcode.mvp.models.Kanji;
-import org.launchcode.mvp.models.dto.KanjiDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
-
-@CrossOrigin (origins = "http://localhost:8080")
 @RestController
 public class KanjiController {
 
     @Autowired
     private KanjiRepository kanjiRepository;
 
-    @PostMapping("/")
-    public ResponseEntity<?> saveKanji(@RequestBody KanjiDTO kanjiDTO, Errors errors,
-                                       HttpSession session) {
-        //error checking
-        if (erros.hasErrors()) {
-            //returns a bad request
-
-            return ResponseEntity.badRequest().body("Validation errors occurred");
-        }
-
-
-        // Turns KanjiDTO to Kanji entity
-        Kanji savedKanji = new Kanji(kanjiDTO.getKanji());
-        //saves entity to the database
-        kanjiRepository.save(savedKanji);
-
-        //return response entity with applicable status
-        return new ResponseEntity<>(kanjiDTO, HttpStatus.OK);
+    @PostMapping("/api/saveKanji")
+    public ResponseEntity<Kanji> saveKanji(@RequestBody Kanji kanjiData) {
+        Kanji savedKanji = kanjiRepository.save(kanjiData);
+        return ResponseEntity.ok(savedKanji);
     }
-
-//   @PutMapping("/updateKanji/{id}")
-//   public ResponseEntity<?> updateKanji(@PathVariable long id, @RequestBody KanjiDTO kanjiDTO) {
-//       // Check if the Kanji exists
-//
-//     String kanji = kanjiDTO.getKanji();
-//       Optional<Kanji> updateKanji = kanjiRepository.findById((int) id);
-//
-//       if (updateKanji.isPresent()) {
-//           updateKanji.get().setKanji(kanjiDTO.getKanji());
-//           kanjiRepository.save(updateKanji.get());
-//       }
-//       return new ResponseEntity<>(kanjiRepository.findByKanji(kanji),HttpStatus.OK);
-//   }
-
 }
--- src/main/java/org/launchcode/mvp/models/Kanji.java
+++ src/main/java/org/launchcode/mvp/models/Kanji.java
@@ -1,41 +1,61 @@
 package org.launchcode.mvp.models;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "kanji_table")
 public class Kanji {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer kanji_id;
+    private Long id;
 
-    @Column(nullable = false)
     private String kanji;
-//    private String vocab;
-//    private String particles;
+    private Integer grade;
+    private String heisigEn;
+    private Integer jlpt;
+    private Integer strokeCount;
+    private String unicode;
+
+    @ElementCollection
+    @CollectionTable(name = "kanji_kun_readings") // Table for kun_readings
+    @Column(name = "reading")
+    private List<String> kunReadings = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "kanji_meanings") // Table for meanings
+    @Column(name = "meaning")
+    private List<String> meanings = new ArrayList<>();
+    // Constructors, getters, and setters
 
 
-    public Kanji() {
-    }
-
-    public Kanji(String kanji){
+    public Kanji(Long id, String kanji, Integer grade, String heisigEn, Integer jlpt, Integer strokeCount, String unicode, List<String> kunReadings, List<String> meanings) {
+        this.id = id;
         this.kanji = kanji;
-//    this.vocab = vocab;
-//    this.particles = vocab;
+        this.grade = grade;
+        this.heisigEn = heisigEn;
+        this.jlpt = jlpt;
+        this.strokeCount = strokeCount;
+        this.unicode = unicode;
+        this.kunReadings = kunReadings;
+        this.meanings = meanings;
+
+
     }
 
-    public Integer getKanji_id() {
-        return kanji_id;
+    public Long getId() {
+        return id;
     }
 
-    public void setKanji_id(Integer kanji_id) {
-        this.kanji_id = kanji_id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getKanji() {
@@ -46,25 +66,59 @@ public class Kanji {
         this.kanji = kanji;
     }
 
-    public void setKanji_id() {
+    public Integer getGrade() {
+        return grade;
     }
 
-    public void setKanji() {
+    public void setGrade(Integer grade) {
+        this.grade = grade;
     }
 
-//    public String getVocab() {
-//        return vocab;
-//    }
-//
-//    public void setVocab(String vocab) {
-//        this.vocab = vocab;
-//    }
-//
-//    public String getParticles() {
-//        return particles;
-//    }
-//
-//    public void setParticles(String particles) {
-//        this.particles = particles;
-//    }
+    public String getHeisigEn() {
+        return heisigEn;
+    }
+
+    public void setHeisigEn(String heisigEn) {
+        this.heisigEn = heisigEn;
+    }
+
+    public Integer getJlpt() {
+        return jlpt;
+    }
+
+    public void setJlpt(Integer jlpt) {
+        this.jlpt = jlpt;
+    }
+
+    public Integer getStrokeCount() {
+        return strokeCount;
+    }
+
+    public void setStrokeCount(Integer strokeCount) {
+        this.strokeCount = strokeCount;
+    }
+
+    public String getUnicode() {
+        return unicode;
+    }
+
+    public void setUnicode(String unicode) {
+        this.unicode = unicode;
+    }
+
+    public List<String> getKunReadings() {
+        return kunReadings;
+    }
+
+    public void setKunReadings(List<String> kunReadings) {
+        this.kunReadings = kunReadings;
+    }
+
+    public List<String> getMeanings() {
+        return meanings;
+    }
+
+    public void setMeanings(List<String> meanings) {
+        this.meanings = meanings;
+    }
 }
