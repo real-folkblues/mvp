2fa4b0c8e13212448a6468286952f54cb0870540
real-folkblues <marenicolor@gmail.com>
2024-03-03 22:33:26 -0500

    added corsmapping and webconfig class

--- src/main/java/org/launchcode/mvp/config/WebConfig.java
+++ src/main/java/org/launchcode/mvp/config/WebConfig.java
@@ -1,20 +1,16 @@
-//package org.launchcode.mvp.config; // Adjust the package name as per your project structure
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000") // For development, you might use "*", but it's more
-//                // secure to
-//                // specify your
-//                // frontend's URL in production
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-//    }
-//}
+package org.launchcode.mvp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
--- src/main/java/org/launchcode/mvp/controllers/KanjiController.java
+++ src/main/java/org/launchcode/mvp/controllers/KanjiController.java
@@ -4,11 +4,12 @@ import org.launchcode.mvp.data.KanjiRepository;
 import org.launchcode.mvp.models.Kanji;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class KanjiController {
 
