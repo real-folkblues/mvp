9f1b8eea9db5ea15c9eff30cd19d86f7bf892835
real-folkblues <marenicolor@gmail.com>
2024-03-09 22:33:14 -0500

    Refactoring form

--- front-end/app/pages-components/login-router/Form.tsx
+++ front-end/app/pages-components/login-router/Form.tsx
@@ -6,6 +6,7 @@ import useSession from "./use-session";
 import { defaultSession } from "./lib";
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
+
 export function Form() {
   const { session, isLoading } = useSession();
 
@@ -27,42 +28,51 @@ export function Form() {
   return <LoginForm />;
 }
 
-function LoginForm() {
+const LoginForm: React.FC = () => {
   const { login } = useSession();
-  const router = useRouter(); //router hook
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+    // Assuming login function returns a promise and result has an isLoggedIn property
+    const result = await login(username, password); // Make sure login accepts password as well
+    if (result.isLoggedIn) {
+      router.push('/user-profile'); // Redirect on successful login
+    }
+  };
 
   return (
-    
-    <div className="">
-    <form
-      onSubmit={async function (event) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const username = formData.get("username") as string;
-        const password = formData.get("password") as string;
-        const result = await login(username); 
-        if (result.isLoggedIn) {
-          router.push('/user-profile');
-         } // Redirect on successful login
-      }}
-      method="POST">
+    <div className='wrapper'>
+      <form onSubmit={handleSubmit} method="POST">
+        <h1>Login</h1>
         <div className="input-box">
-          <input type="text" placeholder='Username' required />
+          <input name="username" type="text" placeholder='Username' required />
+          
         </div>
-      
         <div className="input-box">
-          <input type="text" placeholder='Password' required />
+          <input name="password" type="password" placeholder='Password' required />
+          
         </div>
-      
-      <div className='button'>
-        <input type="submit" value="Login" className='button' />
-      </div>
-    </form>
+
+        <div className="remember-forgot">
+          <label><input type="checkbox" />Remember me</label>
+          <a href="#">Forgot password?</a>
+        </div>
+
+        <button type="submit">Login</button>
+
+        <div className="register-link">
+          <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+      </form>
     </div>
   );
-}
+};
+
+export default LoginForm;
 
 function LogoutButton() {
   const { logout } = useSession();
