42621a79340b1b6069739db3e1a43685ad41287c
real-folkblues <marenicolor@gmail.com>
2024-04-05 16:18:16 -0400

    resolving merge conflict

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -1,6 +1,5 @@
 // import type { Config } from "tailwindcss";
 
-<<<<<<< HEAD
 // const config: Config = {
 //   content: [
 //     "./pages/**/*.{html,js,ts,jsx,tsx,mdx}",
@@ -20,28 +19,6 @@
 //     }
 //   }
 // }
-=======
-const config: Config = {
-  content: [
-    "./pages/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{html,js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" }
-        }
-      },
-      animation: {
-        wiggle: "wiggle 200ms ease-in-out"
-    }
-    
-  }
-}
->>>>>>> Test
 
 
  
